85de90c976a522708f8b784d8b18e43e24976dda
Brett Yarrow <byarrow86@gmail.com>
2019-10-15 21:27:45 -0700

    displayComment() function implemented.

--- sprint-2/scripts/biography.js
+++ sprint-2/scripts/biography.js
@@ -22,24 +22,66 @@ commentAry = [{
 
 ];
 
-function displayComment(comment) {
-    var head = document.getElementsByClassName('conversation__post-container');
-    var post = document.createElement('div');
-    post.className = "conversation__image-box";
-    console.log(post);
-    head.item
-    // head.appendChild(post);
-    for (element in comment) {
-        // console.log(comment[element]); 
-    }
+function dayFormat(day) {
+    var dayArray = [];
+
+    dayArray.push((day.getMonth() + 1) < 10 ? '0' + (day.getMonth() + 1) : (day.getMonth() + 1));
+    dayArray.push(day.getDate());
+    dayArray.push(day.getFullYear());
+
+    return dayArray.join('/');
+}
+
+function displayComment(entry) {
+    //create comment structure
+    var head = document.getElementsByClassName('conversation__post-container')[0];
+    var newPost = document.createElement('div');
+    var imgBox = document.createElement('div');
+    var img = document.createElement('img');
+    var textBox = document.createElement('div');
+    var header = document.createElement('header');
+    var name = document.createElement('h2');
+    var date = document.createElement('h5');
+    var comment = document.createElement('p');
+
+
+    //assign classes
+    newPost.setAttribute('class', 'conversation__post');
+    imgBox.setAttribute('class', 'conversation__image-box');
+    img.setAttribute('class', 'conversation__image');
+    img.setAttribute('src', './assets/images/blank.jpg');
+    textBox.setAttribute('class', 'conversation__text-box');
+    header.setAttribute('class', 'conversation__header');
+    name.setAttribute('class', 'conversation__name');
+    date.setAttribute('class', 'conversation__date');
+    comment.setAttribute('class', 'conversation__comment');
+
+    //add content
+    name.innerHTML = entry['name'];
+    date.innerHTML = dayFormat(entry['date']);
+    comment.innerHTML = entry['comment'];
+
+
+    //append together
+    imgBox.appendChild(img);
+    newPost.appendChild(imgBox);
+
+    header.appendChild(name);
+    header.appendChild(date);
+
+    textBox.appendChild(header);
+    textBox.appendChild(comment);
+
+    newPost.appendChild(textBox);
+
+    head.appendChild(newPost);
 }
 
 for (let i = 0; i < commentAry.length; i++) {
-    // const element = commentAry[i];
     displayComment(commentAry[i]);
 
 }
 
-
+// displayComment(commentAry[0]);
 
 // console.log(commentAry);
